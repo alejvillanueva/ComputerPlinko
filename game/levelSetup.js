a30ff45f361181a200cc90ee27e8ed3ed3d1e46c
drasis <doron.rasis@nyu.edu>
2019-04-28 19:11:35 -0400

    accounts for new ball starting position and variable-width levels

--- game/levelSetup.js
+++ game/levelSetup.js
@@ -4,13 +4,17 @@ function levelSetup(l) {
     return ;
   }
   level = l;
+  box_col = levels[level]["box_col"];
+  // ball = new Ball(boxheight * (floor(box_row/2) + .5), 0, boxheight);
+  ball.setColumn(levels[l]["ball_start"]);
+  console.log(ball.column())
   resetGrid();
   // console.log(levels[l]["permitted"]);
   // permitted = Object.assign(levels[l]["permitted"], permitted);
   permitted = deepcopy(levels[l]["permitted"]);
-  console.log(permitted);
+  // console.log(permitted);
   goal = deepcopy(levels[l]["goal"]);
-  console.log(levels[l]["boxes"]);
+  // console.log(levels[l]["boxes"]);
   for (var i = 0; i < levels[l]["boxes"].length; i++) {
       boxes[levels[l]["boxes"][i][0]][levels[l]["boxes"][i][1]] = new boxObj(levels[l]["boxes"][i][2], levels[l]["boxes"][i][3]);
   }
@@ -19,6 +23,7 @@ function levelSetup(l) {
 
 function resetGrid() {
   for (var x = 0; x < box_row; x++) {
+  // for (var x = 0; x < box_col; x++) {
     for (var y = 1; y < box_row; y++) {
       boxes[x][y] = new boxObj();
     }
